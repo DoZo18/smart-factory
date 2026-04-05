1b56b3a5d336b71bf710b807994f5e69dab34524
DoZo <nongdose@gmail.com>
2026-04-05 17:38:03 +0700

    adjust layout dashboard page

--- backend/sensor/views.py
+++ backend/sensor/views.py
@@ -7,23 +7,26 @@ def dashboard(request):
     data      = SensorData.objects.all().order_by('id')[:limit]
     data_list = list(data)
 
-    air_temp     = [d.air_temperature for d in data_list]
-    process_temp = [d.process_temperature for d in data_list]
-    machine_fail = [d.machine_failure for d in data_list]
-    labels       = [str(d.id) for d in data_list]
+    air_temp          = [d.air_temperature for d in data_list]
+    process_temp      = [d.process_temperature for d in data_list]
+    rotational_speed  = [d.rotational_speed for d in data_list]
+    machine_fail      = [d.machine_failure for d in data_list]
+    labels            = [str(d.id) for d in data_list]
 
     context = {
         # HTML ใช้
-        'limit':            limit,
-        'data':             data,
-        'avg_air_temp':     round(sum(air_temp) / len(air_temp), 2) if air_temp else "N/A",
-        'avg_process_temp': round(sum(process_temp) / len(process_temp), 2) if process_temp else "N/A",
-        'total_fail':       sum(machine_fail),
+        'limit':                limit,
+        'data':                 data,
+        'avg_air_temp':         round(sum(air_temp) / len(air_temp), 2) if air_temp else "N/A",
+        'avg_process_temp':     round(sum(process_temp) / len(process_temp), 2) if process_temp else "N/A",
+        'avg_rotational_speed': round(sum(rotational_speed) / len(rotational_speed), 2) if rotational_speed else "N/A",
+        'total_fail':           sum(machine_fail),
+        'failure_rate':         f"{round(sum(machine_fail) / len(machine_fail) * 100, 2)}%" if machine_fail else "N/A",
         # JavaScript ใช้
-        'labels':           json.dumps(labels),
-        'air_temp':         json.dumps(air_temp),
-        'process_temp':     json.dumps(process_temp),
-        'machine_fail':     json.dumps(machine_fail),
+        'labels':               json.dumps(labels),
+        'air_temp':             json.dumps(air_temp),
+        'process_temp':         json.dumps(process_temp),
+        'machine_fail':         json.dumps(machine_fail),
     }
-    print(data)
     return render(request, 'sensor/dashboard.html', context)
+
